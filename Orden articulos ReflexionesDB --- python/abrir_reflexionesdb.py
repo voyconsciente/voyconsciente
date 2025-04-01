52d4a3812f811bd4a12166b9853ddedf7224667e
Sebastian Redigonda <sebastianredigonda@Mac-mini-de-Sebastian.local>
2025-04-01 16:46:41 -0300

    Conecté el servidor local a una base de datos externa y limpié archivos innecesarios

--- Orden articulos ReflexionesDB --- python/abrir_reflexionesdb.py
+++
@@ -1,7 +0,0 @@
-from app import app, db, Reflexion  # Asegúrate de importar bien estos módulos
-
-with app.app_context():
-    reflexiones = Reflexion.query.all()  # Ahora la base de datos funcionará correctamente
-
-    for r in reflexiones:
-        print(r.id, r.titulo, r.categoria, r.fecha)
